45530a0a124cf35f60874609eccdba493ce4e23b
Victor Lamoine <victor.lamoine@gmail.com>
2016-01-19 15:52:45 +0100

    Build #8

--- .travis.sh
+++ .travis.sh
@@ -1,4 +1,8 @@
+pwd
+ls
 cd ..
+pwd
+ls
 git clone https://github.com/ros-industrial/fanuc
 git clone https://github.com/ros-industrial/fanuc_experimental
-cd institut_maupertuis_robots_descrptions
+cd institut_maupertuis_robots_descriptions
